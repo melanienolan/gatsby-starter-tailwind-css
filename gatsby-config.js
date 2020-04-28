6f02f73728998114e388c161e80d987e0f193969
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-28 11:56:08 +0100

    add config for postcss and purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [require("tailwindcss"), require("autoprefixer")],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-purgecss",
+      options: {
+        printRejected: false,
+        develop: false,
+        tailwind: true,
+      },
+    },
+  ],
 };
