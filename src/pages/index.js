0470b40e298cac0c78801b4821d2895ecf40c8d6
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-29 17:49:45 +0100

    use layout and seo components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Layout from "../components/Layout.js";
+import SEO from "../components/SEO.js";
 
 export default () => {
   return (
-    <>
+    <Layout>
+      <SEO />
       <h1 className="text-xl text-center text-teal-500">Hello</h1>
-    </>
+      <button className="bg-purple-700 p-4 hover:bg-purple-300">
+        This is a button
+      </button>
+    </Layout>
   );
 };
