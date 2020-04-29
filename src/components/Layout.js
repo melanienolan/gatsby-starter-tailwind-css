47cbfef5107413266a0d46fe2e6f1d1112f65f9a
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-29 17:48:34 +0100

    create layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <header></header>
+      <main>{children}</main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default Layout;
