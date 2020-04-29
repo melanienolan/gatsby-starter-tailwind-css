0324a90468dd982c54806936cd678e3e3f8240e8
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-29 17:46:54 +0100

    add site metadata and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,25 @@
 module.exports = {
+  siteMetadata: {
+    title: "Gatsby Starter TailwindCSS",
+    author: {
+      name: "Melanie Nolan",
+    },
+    description: "A Gatsby starter to set you up with TailwindCSS",
+    siteUrl: "https://www.google.com",
+  },
   plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Gatsby Starter TailwindCSS",
+        short_name: "Gatsby Starter TailwindCSS",
+        start_url: "/",
+        background_color: "#38b2ac",
+        theme_color: "#38b2ac",
+        icon: "src/images/icon.svg",
+      },
+    },
     {
       resolve: "gatsby-plugin-postcss",
       options: {
