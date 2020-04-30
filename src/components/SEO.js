1f40a8a9e54c7372f3f2a410734bb73f58eb2b3b
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-30 16:50:06 +0100

    remove duplicate

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -13,7 +13,6 @@ const SEO = () => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
-      <meta property="description" content={description} />
       <title>{title}</title>
     </Helmet>
   )
