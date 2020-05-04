01511742bbfb7642a3b002aede8c6f94489d8088
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-04 12:04:29 +0100

    style layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,9 +5,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <SEO />
-      <header></header>
-      <main>{children}</main>
-      <footer></footer>
+      <div className="flex flex-col min-h-screen bg-gray-200">
+        {children}
+        <footer className="py-2 text-center text-gray-600 text-xs">
+          &copy; Melanie Nolan 2020
+        </footer>
+      </div>
     </>
   )
 }
