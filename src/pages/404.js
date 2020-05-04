0cd3ac4ce0ecff568defab2769f284f36536e26d
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-04 18:01:12 +0100

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/Layout.js"
+import notFoundImage from "../images/not-found-image.svg"
+
+export default () => {
+  return (
+    <Layout>
+      <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around">
+        <img className="p-4" src={notFoundImage} alt="Page not found" />
+      </main>
+    </Layout>
+  )
+}
