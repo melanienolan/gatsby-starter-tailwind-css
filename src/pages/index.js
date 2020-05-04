ee1ad859d9578ac89f72541a9f19f018e27f58fc
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-04 17:47:26 +0100

    updates to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,35 @@ import heroImage from "../images/hero-image.svg"
 export default () => {
   return (
     <Layout>
-      <main className="container flex-grow mx-auto">
-        <div>
-          <h1 className="px-4 pt-5 text-2xl text-left text-teal-500 font-bold">
-            Gatsby Starter Tailwind CSS
-          </h1>
-          <p className="px-4 mt-8 text-lg text-gray-700">
-            Start your Gatsby and Tailwind CSS project easily with this Gatsby
-            Starter
-          </p>
+      <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around">
+        <div className="sm:flex sm:flex-row-reverse sm:items-center">
+          <div className="sm:px-2">
+            <h1 className="px-4 pt-5 text-2xl text-left text-teal-500 font-bold sm:text-3xl">
+              Gatsby Starter Tailwind CSS
+            </h1>
+            <p className="px-4 mt-8 text-lg text-gray-700 sm:mt-8">
+              Start your Gatsby and Tailwind CSS project easily with this Gatsby
+              Starter
+            </p>
+          </div>
+          <img
+            className="w-full max-w-lg mt-16 mx-auto sm:w-1/2"
+            src={heroImage}
+            alt="Gatsby and Tailwind CSS together"
+          />
         </div>
-        <img
-          className="mt-12 w-full"
-          src={heroImage}
-          alt="Gatsby and Tailwind CSS together"
-        />
-        <div className="p-3 mt-12 mx-2 bg-white rounded shadow-xs">
-          <code className="text-gray-600 text-xs leading-3">
+
+        <div className="mx-auto">
+          <a
+            href="https://github.com/melanienolan/gatsby-starter-tailwindcss"
+            className="p-4 bg-teal-500 text-white text-xs rounded shadow hover:bg-teal-400"
+          >
+            View on GitHub
+          </a>
+        </div>
+
+        <div className="flex items-center p-3 mx-2 bg-white rounded shadow-xs sm:mx-auto">
+          <code className="text-gray-600 text-xs leading-xl">
             gatsby new my-project-name
             https://github.com/melanienolan/gatsby-starter-tailwindcss
           </code>
