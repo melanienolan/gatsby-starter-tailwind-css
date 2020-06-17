339ddff0ffc73a7f4aab20df86f4f8b855b57cb6
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-17 17:30:06 +0100

    tidy import statements

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout.js"
+import Layout from "../components/Layout"
 import notFoundImage from "../images/not-found-image.svg"
 
 export default () => {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout.js"
+import Layout from "../components/Layout"
 import heroImage from "../images/hero-image.svg"
 
 export default () => {
